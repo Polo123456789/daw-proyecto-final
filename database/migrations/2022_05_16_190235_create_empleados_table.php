6f4522453ca8d71b86e131da3ad2e002a2adc850
polo123456789 <pabsan02@gmail.com>
2022-05-16 13:07:06 -0600

    (+) Tabla empleados

---
+++ database/migrations/2022_05_16_190235_create_empleados_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('empleados', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->string('nombre');
+            $table->string('correo');
+            $table->string('telefono');
+            $table->string('direccion');
+            $table->string('turno');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('empleados');
+    }
+};
