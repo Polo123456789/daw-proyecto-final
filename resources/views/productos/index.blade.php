a2b184c8d9f70236cf58770ae1771df521c1610e
polo123456789 <pabsan02@gmail.com>
2022-05-15 19:00:42 -0600

    Terminado el display de las tablas en el dashboard

--- resources/views/productos/index.blade.php
+++ resources/views/productos/index.blade.php
@@ -1,35 +1,38 @@
 <x-app-layout>
-    <div class="w-full flex justify-end">
-        <x-link-button class="text-black border-black hover:text-white hover:bg-black">
+    <div class="w-full flex">
+        <x-link-button class="text-white bg-gray-700 border-slate-800 hover:bg-slate-900">
             Crear nuevo Producto
         </x-link-button>
     </div>
-    
-    <table class="w-full text-sm text-left text-gray-500">
+
+    <table class="w-full text-sm text-gray-500 text-center">
         <thead class="text-xs text-gray-700 uppercase bg-gray-50">
-            <th class="text-center">Id</th>
-            <th class="text-center">Nombre</th>
-            <th class="text-center">Precio</th>
-            <th class="text-center">Existencias</th>
+            <th>Id</th>
+            <th>Nombre</th>
+            <th>Precio</th>
+            <th>Existencias</th>
             <th></th>
             <th></th>
         </thead>
         <tbody>
             <tr class="bg-white border-b">
-                <td class="px-6 py-4 text-center">1</td>
-                <td class="px-6 py-4 text-center">Manzanas</td>
-                <td class="px-6 py-4 text-center">10.00</td>
-                <td class="px-6 py-4 text-center">50</td>
-                <td class="px-6 py-4 text-center">
+                <td class="px-6 py-4">1</td>
+                <td class="px-6 py-4">Manzanas</td>
+                <td class="px-6 py-4">10.00</td>
+                <td class="px-6 py-4">50</td>
+                <td class="px-6 py-4">
                     <x-link-button class="text-black border-black hover:text-white hover:bg-black">
                         Modificar
                     </x-link-button>
                 </td>
-                <td class="px-6 py-4 text-center">
+                <td class="px-6 py-4">
                     <x-link-button class="text-black border-black hover:text-white hover:bg-black">
                         Eliminar
                     </x-link-button>
                 </td>
+            </tr>
+            <tr class="bg-white border-b">
+                <td colspan="6" class="px-6 py-4">Aun no hay datos</td>
             </tr>
         </tbody>
     </table>
