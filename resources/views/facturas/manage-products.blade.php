263b7af6ff4c0be233ba8d01a75776553f4e1d15
polo123456789 <pabsan02@gmail.com>
2022-05-27 19:17:50 -0600

    (-) Error solucionado

--- resources/views/facturas/manage-products.blade.php
+++ resources/views/facturas/manage-products.blade.php
@@ -16,7 +16,7 @@
                     </select>
                     <div></div>
                     <label>Cantidad:</label>
-                    <input type="number" x-model="quantitySelected" class="grow bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
+                    <input type="number" x-model.number="quantitySelected" class="grow bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5">
                     <button
                         class="border p-2 rounded text-white bg-gray-700 border-slate-800 hover:bg-slate-900"
                         @click="addProduct"
